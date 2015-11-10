f7891cd4ed5193cd3a053d74a2e4533cad463409
qiwsir <qiwsir@gmail.com>
2015-11-10 10:08:05 +0800

    main

---
+++ basestat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+#import numpy as np
+
+def base_average(arry):
+    """
+    someting
+    """
+    #result = np.mean(arry)
+    r_sum = sum(arry)
+    result = r_sum*1.0 / len(arry)
+    return result
---
+++ main.py
@@ -0,0 +1,8 @@
+# /usr/bin/env python
+# coding:utf-8
+
+import basestat
+
+
+a = [1, 3, 5, 6]
+print basestat.base_average(a)
