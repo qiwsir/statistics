164ef096d970b721df41f587af05ed269c9486dc
linbin <binlin1990@126.com>
2015-11-12 18:12:01 +0800

    variance quantity

---
+++ variquanl.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import numpy as np
+
+def average_deviation(arr):
+         
+           arr.sort()
+           if len(arr)%2==1:
+               res = abs(np.array(arr)-arr[len(arr)/2])
+           else:
+               res = abs(np.array(arr)-(arr[len(arr)/2]+arr[len(arr)/2-1])/2.0)
+                    
+           result = 1.0*res.sum()/len(arr)
+           return result
+
+def standard_deviation(arr):
+         result = np.std(arr)
+         return result
+
+def variance(arr):
+         result = np.var(arr)
+         return result
+
+def variation_coefficient(arr):
+         result = 1.0*np.std(arr)/np.mean(arr)
+         return "%.2f%%"%(result*100)
